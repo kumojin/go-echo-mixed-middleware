c6abe2a520ccbd3678930283a17bb3355d087e4b
Roman Serikov <roman.serikov@yandex.ru>
2021-10-01 02:26:46 +0300

    Refactored tests, removed testify/assert

--- middleware_test.go
+++ middleware_test.go
@@ -7,49 +7,77 @@ import (
 	"testing"
 
 	echo "github.com/labstack/echo/v4"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestMixedAuthenticationSucceedsOnFirstAuth(t *testing.T) {
-	auth1 := succeedingMiddleware("hello")
-	auth2 := failingMiddleware(errors.New("boom"))
+func TestMixed(t *testing.T) {
+	type args struct {
+		mw1 echo.MiddlewareFunc
+		mw2 echo.MiddlewareFunc
+	}
 
-	ctx := newContext()
-	md := Mixed()(auth1, auth2)
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "TestMixedAuthenticationSucceedsOnFirstAuth",
+			args: args{
+				mw1: succeedingMiddleware("hello"),
+				mw2: failingMiddleware(errors.New("boom")),
+			},
+			want: "hello",
+		},
+		{
+			name: "TestMixedAuthenticationSucceedsOnSecondAuth",
+			args: args{
+				mw1: failingMiddleware(errors.New("boom")),
+				mw2: succeedingMiddleware("hello2"),
+			},
+			want: "hello2",
+		},
+		{
+			name: "TestMixedAuthenticationFailsOnBothFailedAuths",
+			args: args{
+				mw1: failingMiddleware(errors.New("boom")),
+				mw2: failingMiddleware(errors.New("boom2")),
+			},
+			want:    "boom2",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				got string
+				ctx = newContext()
+				md  = Mixed()(tt.args.mw1, tt.args.mw2)
 
-	handlerFunc := md(nil)
-	err := handlerFunc(ctx)
+				handlerFunc = md(nil)
+				err         = handlerFunc(ctx)
+			)
 
-	assert.NoError(t, err)
-	assert.Equal(t, "hello", ctx.Get("KEY"))
-}
+			switch tt.wantErr {
+			case true:
+				if err == nil {
+					t.Errorf("got %v, want %v", err, tt.wantErr)
+				}
 
-func TestMixedAuthenticationSucceedsOnSecondAuth(t *testing.T) {
-	auth1 := failingMiddleware(errors.New("boom"))
-	auth2 := succeedingMiddleware("hello2")
+				got = err.Error()
+			default:
+				if err != nil {
+					t.Errorf("got %v, want %v", err, tt.wantErr)
+				}
 
-	ctx := newContext()
-	md := Mixed()(auth1, auth2)
+				got = ctx.Get("KEY").(string)
+			}
 
-	handlerFunc := md(nil)
-	err := handlerFunc(ctx)
-
-	assert.NoError(t, err)
-	assert.Equal(t, "hello2", ctx.Get("KEY"))
-}
-
-func TestMixedAuthenticationFailsOnBothFailedAuths(t *testing.T) {
-	auth1 := failingMiddleware(errors.New("boom"))
-	auth2 := failingMiddleware(errors.New("boom2"))
-
-	ctx := newContext()
-	md := Mixed()(auth1, auth2)
-
-	handlerFunc := md(nil)
-	err := handlerFunc(ctx)
-
-	assert.Equal(t, "boom2", err.Error())
+			if got != tt.want {
+				t.Errorf("Mixed() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
 func succeedingMiddleware(succeedingValue string) echo.MiddlewareFunc {
